c94eba3325dc2c3ac84c10182b846c4d616cf4b9
tony84727 <tony84727@gmail.com>
2019-06-27 07:36:15 +0800

    replace example mod

---
+++ src/main/java/com/gitlab/tony84727/advancedweaponry/AdvancedWeaponry.java
@@ -0,0 +1,16 @@
+package com.gitlab.tony84727.advancedweaponry;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid=AdvancedWeaponry.ModID, name = AdvancedWeaponry.Name, version = AdvancedWeaponry.Version)
+public class AdvancedWeaponry
+{
+	public static final String ModID = "advancedweaponry";
+	public static final String Name = "Advanced Weaponry";
+	public static final String Version  = "0.0.0";
+
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent e) {
+	}
+}
