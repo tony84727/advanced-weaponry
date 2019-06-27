60a148a646e0f354c1b2ab1b97ce7fc9bb420b5b
tony84727 <tony84727@gmail.com>
2019-06-27 17:57:52 +0800

    add rock texture

--- src/main/java/com/gitlab/tony84727/advancedweaponry/item/Items.java
+++ src/main/java/com/gitlab/tony84727/advancedweaponry/item/Items.java
@@ -1,9 +1,12 @@
 package com.gitlab.tony84727.advancedweaponry.item;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod.EventBusSubscriber
 public class Items
@@ -11,10 +14,13 @@ public class Items
 	public static final Rock Rock = new Rock();
 
 	@SubscribeEvent
-	public static void onRegisterItems(RegistryEvent.Register<Item> register)
+	public static void onRegisterItems(RegistryEvent.Register<Item> e)
 	{
-		register.getRegistry().registerAll(
-				Rock
-		);
+		registerItem(e.getRegistry(), Rock);
+	}
+
+	private static void registerItem(IForgeRegistry<Item> registry,Item item) {
+		ModelLoader.setCustomModelResourceLocation(item, 0,new ModelResourceLocation(item.getRegistryName(),"inventory"));
+		registry.register(item);
 	}
 }
